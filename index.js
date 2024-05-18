86a078afa8c1f2f11c918242b3c447187b5e96c4
Jas Singh Ajmani <164511768+jas001469@users.noreply.github.com>
2024-05-18 14:40:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const decreseBtn= document.getElementById("decreaseBtn");
+const resetBtn= document.getElementById("resetBtn");
+const increaseBtn= document.getElementById("increaseBtn");
+const countLable = document.getElementById("countLable");
+ let count = 0;
+
+ increaseBtn.onclick = function(){
+    count++;
+    countLable.textContent = count;
+ }
+decreseBtn.onclick= function(){
+    count--;
+    countLable.textContent = count;
+}
+resetBtn.onclick= function(){
+    count=0;
+    countLable.textContent = count;
+}
